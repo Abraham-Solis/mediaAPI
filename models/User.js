86a676956dd942fc9f6bbbe125c9b49796f5dfbc
email <abe.solis2015@gmail.com>
2022-01-13 14:42:58 -0800

    User Model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,34 @@
-const { Shcema, model} = require ('mongoose')
+const { Schema, model} = require ('mongoose')
+
+const User = new Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+      },
+      message: "Please enter a valid email"
+    },
+    required: [true, "Email required"]
+  },
+  thought: [{
+    type: Shcema.Types.ObjectId,
+    ref: 'thought',
+  }],
+  friend: [{
+    tyep: Schema.Types.ObjectId,
+    ref 'friend'
+  }]
+
+  })
+
+module.exports = model('user', User)
