aecf9cd7552122f7a75a327658a4f86f83378a88
email <abe.solis2015@gmail.com>
2022-01-13 20:08:54 -0800

    Reaction Code

--- routes/reactionRoutes.js
+++ routes/reactionRoutes.js
@@ -3,16 +3,16 @@ const { Reaction, Thought, User } = require('../models')
 
 //POST ONE Reaction 
 router.post('/reactions', async function (req, res) {
-  const thought = await Thought.create(req.body)
-  await User.findByIdAndUpdate(req.body.user, {
+  const reaction = await Reaction.create(req.body)
+  await Reaction.findByIdAndUpdate(req.body.user, {
     $push: { reactions: reaction._id }
   })
-  res.json(thought)
+  res.json(reaction)
 })
 
 //DELETE one Reaction by id
 router.delete('/reactions/:id', async function (req, res) {
-  await Thought.findByIdAndDelete(req.params.id)
+  await Reaction.findByIdAndDelete(req.params.id)
   res.sendStatus(200)
 })
 
